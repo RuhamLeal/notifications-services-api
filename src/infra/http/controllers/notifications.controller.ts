aa93d073819ef53056498f1877c8ff5418cfc673
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-17 22:47:33 -0300

    feat: implementing send notifications into notifications-controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,21 @@
+import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '@infra/create-notification-body';
 import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async createNotification(@Body() body: CreateNotificationBody) {
     const { recipientId, category, content } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
   }
 }
