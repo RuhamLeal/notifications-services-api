e4607a821509317a7e5b11432c6e9664d3b1a889
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-18 16:48:59 -0300

    test: adding test to cancel-notification use-case when the notificatio does not exists

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,9 +2,10 @@ import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repositories';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found-error';
 
 describe('Cancel Notification', () => {
-  it('Should be able to send a notification', async () => {
+  it('Should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -23,5 +24,16 @@ describe('Cancel Notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('Should not be able to cancel a notification when it does not exists', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
