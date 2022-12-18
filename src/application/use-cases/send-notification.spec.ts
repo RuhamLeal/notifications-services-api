7febdb5b9f3023e77e02808514f727f159afccb8
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-17 21:46:52 -0300

    test: testing if the send-notification funcionality save notification in 'notificationsRepository'

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('Should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'A new Notification',
       category: 'Social',
       recipientId: '12341FAFA543DS',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
