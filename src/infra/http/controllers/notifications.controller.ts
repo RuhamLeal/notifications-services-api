e6ee6c15b98f6e5946d2a3ada78969f025e4c4cc
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-22 14:03:03 -0300

    feat: adding controller to cancel a notification by its ID

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,22 @@
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '@infra/create-notification-body';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Patch, Param } from '@nestjs/common';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
 
   @Post()
   async createNotification(@Body() body: CreateNotificationBody) {
